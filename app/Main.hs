64aeca95372c4fed20308de10c522c11667dd017
Michael Snoyman <michael@snoyman.com>
2018-08-26 10:32:24 +0300

    Templates

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,9 @@ import Yesod.EventSource
 import Network.Wai.EventSource
 import Data.Text.Encoding (encodeUtf8Builder)
 import Data.Aeson (toEncoding, fromEncoding, Value (String))
+import Text.Hamlet (hamletFile)
+import Text.Lucius (Css, luciusFile)
+import Text.Julius (Javascript, juliusFile)
 
 data Result = Result !Word !Text
 
@@ -27,77 +30,29 @@ data App = App
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
+/static/style.css StyleR GET
+/static/script.js ScriptR GET
 /submit SubmitR PUT
 /result ResultR GET
 |]
 
-instance Yesod App
+instance Yesod App where
+  makeSessionBackend _ = pure Nothing
 
 title :: Text
 title = "Kids Coding Haskell IDE"
 
 getHomeR :: Handler Html
-getHomeR = do
-  render <- getUrlRenderParams
-  pure $ ($ render) [hamlet|
-$doctype 5
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>#{title}
+getHomeR =
+  $(hamletFile "template/home.hamlet") <$> getUrlRenderParams
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.39.2/codemirror.min.css" crossorigin=anonymous>
+getStyleR :: Handler Css
+getStyleR =
+  $(luciusFile "template/home.lucius") <$> getUrlRenderParams
 
-    <style>
-      #editor-wrapper {
-        border: 1px solid black;
-        padding: 0.5em;
-        margin: 0.5em;
-      }
-
-      #result {
-        margin: 0.5em;
-        padding: 0.5em;
-        background: #f8f8f8;
-      }
-
-  <body>
-
-    <div .container>
-      <div .row>
-        <h1>#{title}
-
-      <div .row>
-        <div .col-sm-6>
-          <div #editor-wrapper>
-            <textarea #editor>main = print "Hello World!"
-        <div .col-sm-6>
-          <pre #result>No results yet
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.39.2/codemirror.min.js" crossorigin=anonymous>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.39.2/mode/haskell/haskell.js" crossorigin=anonymous>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-
-    <script>
-      var cm = CodeMirror.fromTextArea(document.getElementById("editor"),
-            { autofocus: true
-            , mode: "haskell"
-            }),
-          submit = function() {
-                     fetch("@{SubmitR}", {method: "PUT", body: cm.getValue()})
-                   },
-          result = document.getElementById("result")
-          ;
-      cm.on('change', submit);
-      submit();
-
-      new EventSource("@{ResultR}").onmessage = function(e) {
-        result.innerText = JSON.parse(e.data);
-      };
-|]
+getScriptR :: Handler Javascript
+getScriptR = do
+  $(juliusFile "template/home.julius") <$> getUrlRenderParams
 
 putSubmitR :: Handler ()
 putSubmitR = do
